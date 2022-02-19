608daf6805e03abf097cab54c03a03a9716d794b
Sean Cruz <seankennethcruz@gmail.com>
2022-02-19 15:30:13 -0800

    added constants for pw

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+// mixing symbols, lowercase and uppercase letters, and numbers into constants
+const symbols = "!@#$%^&*_-+=";
+const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "0123456789";
+
+
+
 document.getElementById('generate').addEventListener('click', () => {
 
 })
