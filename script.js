16e32304f3a8c009bbc314164d8a287205e5550a
Sean Cruz <seankennethcruz@gmail.com>
2022-02-21 13:09:51 -0800

    commit confirmation alerts

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const generateBtn = document.getElementById("generate");
 //Defining variables for what the password will include
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 var specChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "+", "="];
 
 var addLength = "";
@@ -23,6 +23,22 @@ function generatePassword() {
   }
 
 
+  //Alert shown when length of password fits 8-128 character criteria
+  alert("Your password will be " + addLength + " characters long.");
+
+  //Confirming what characters should be included in the password
+  var addLowerCase = confirm("Do you want your Password to include lowercase characters? Press 'OK' to add and 'Cancel' to remove.");
+  var addUpperCase = confirm("Do you want your Password to include uppercase characters? Press 'OK' to add and 'Cancel' to remove.");
+  var addNumbers = confirm("Do you want your Password to include numbers? Press 'OK' to add and 'Cancel' to remove.");
+  var addSpecChar = confirm("Do you want your Password to include special characters? Press 'OK' to add and 'Cancel' to remove.");
+
+  //Alert shown when none of the criteria are selected
+  while (
+    addLowerCase === false &&
+    addUpperCase === false &&
+    addNumbers === false &&
+    addSpecChar === false) {
+    alert("Please select at least ONE of the criteria to generate your password.");
 
 // document.getElementById('generate').addEventListener('click', () => {
 
